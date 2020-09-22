0721ed3205476c4354f110b1dc66e3b9b8ee3ef4
oliver <1999ju373@gmail.com>
2020-09-22 14:08:40 +0900

    osa_k

--- algorism for competiton programing/osa_k_ABC179C.cpp
+++ algorism for competiton programing/osa_k_ABC179C.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
+//#include<atcoder/all>
 // Begin Header {{{
+//using namespace atcoder;
 using namespace std;
 using ll = long long;
 using P = pair<ll, ll>;
@@ -16,30 +18,36 @@ template<class T> bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } r
 template<class T> bool chmin(T &a, const T &b) { if (a>b) { a = b; return 1; } return 0; }
 // }}} End Header
 
-
+std::vector<ll> Eratos_sieve(ll n){
+	//[0, n]のインデックスに対して、最小の素因数を記録するリスト
+	vector<ll> sieve(n);
+	iota(sieve.begin(), sieve.end(), 0);
+	for(ll i=2; i*i<n; ++i){
+		if(sieve[i] < i) continue;
+		for(ll j=i*i; j<n; j+=i){
+			if(sieve[j] == j) sieve[j] = i;
+		}
+	}
+	return sieve;
+}
 int main() {
 	ll n, ans = 0;
 	cin >> n;
-	vector<ll> sieve(n);
-	loop(i, 2, n){
-		for(ll j=i; j<n; j+=i){
-			if(sieve[j] == 0) sieve[j]=i;
-		}
-	}
+	vector<ll> sieve = Eratos_sieve(n);
 	
 	for(ll c=1; c<n; c++){
 		ll x = n-c;
-		ll subans=1;
-		map<ll, ll> tmp;
+		ll divisors=1;
+		map<ll, ll> cnt;
 
 		while(x > 1){
-			tmp[sieve[x]]++;
+			cnt[sieve[x]]++;
 			x/=sieve[x];
 		}
-		for(auto x : tmp){
-			subans*=(x.second+1);
+		for(auto x : cnt){
+			divisors*=(x.second+1);
 		}
-		ans+=subans;
+		ans+=divisors;
 	}
 	cout << ans << endl;
 	return 0;
