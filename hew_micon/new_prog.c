459c5edaf91b37c9e9d3049efd54426b11295ad6
oliver <1999ju373@gmail.com>
2020-09-25 13:17:43 +0900

    new

---
+++ hew_micon/new_prog.c
@@ -0,0 +1,62 @@
+#ifdef __cplusplus
+extern "C" {
+void abort(void);
+#endif
+void main(void);
+#ifdef __cplusplus
+}
+#endif
+
+/*インクルード***********************************************************/
+#include<36064s.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include "vs-wrc003lv.h"
+
+/*マクロ***********************************************************/
+
+/*グローバル変数***********************************************************/
+int cnt = 0;
+int black_cnt=0;
+int judge = 0;
+/*メイン関数***********************************************************/
+void main(void)
+{
+	//制御周期の設定[単位：Hz　範囲：30.0~]
+	const BYTE MainCycle = 60;
+	
+	Init((BYTE)MainCycle);		//CPUの初期設定
+	//I2C_init(void);	//IXBUS初期化
+	
+	//ループ
+	
+	while(1){
+		const int border = 500;
+		int right = AdRead(0);
+		int left = AdRead(1);
+		
+		if(right > border){
+			Mtr_Run_lv(10000, -10000,0,0,0,0);
+			Wait(100);
+		}
+		if(left > border){
+			Mtr_Run_lv(-10000, 10000,0,0,0,0);
+			Wait(100);
+		}else{
+			Mtr_Run_lv(0, 0,0,0,0,0);
+			Wait(100);
+			Mtr_Run_lv(-10000, -10000,0,0,0,0);
+			Wait(100);
+		}
+			
+	}
+
+}
+#ifdef __cplusplus
+void abort(void)
+{
+	
+}
+#endif
